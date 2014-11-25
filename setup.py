8967bb0e0855f6350ce6804c0f8e723953a2e02c
cgtx <carl@carlgeorge.us>
2014-11-25 15:21:57 -0600

    add python version check

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
+import sys
+
+
+if not sys.version_info >= (3, 4):
+    sys.exit('requires Python 3.4 or newer')
+
 
 setuptools.setup(
-    name = 'toleo',
-    version = '0.0.2',
-    description = 'Library for tracking software and package versions.',
-    author = 'Carl George',
-    author_email = 'carl@cgtx.us',
-    url = 'https://github.com/cgtx/toleo',
-    packages = ['toleo'],
-    install_requires = ['requests', 'setuptools', 'beautifulsoup4', 'PyYAML']
+    name='toleo',
+    version='0.0.2',
+    description='Library for tracking software and package versions.',
+    author='Carl George',
+    author_email='carl@cgtx.us',
+    url='https://github.com/cgtx/toleo',
+    packages=['toleo'],
+    install_requires=['requests', 'setuptools', 'beautifulsoup4', 'PyYAML']
 )
